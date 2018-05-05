412c91a34e1d1a1a2ec1d37cee744af289f51884
Adam Mahood <mahood.adam@gmail.com>
2018-05-05 00:07:56 -0400

    WIP Commit of refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <iomanip>
 
 struct transaction {
 	int id;
@@ -9,32 +10,60 @@ struct transaction {
 	std::string date_created;
 };
 
+std::string get_date();
+transaction create_transact(const int&);
+void print_transact(std::ostream&, const transaction&);
+
 int main() {
-	transaction transac;
-	std::cout << "Create new transaction: " << std::endl;
-	std::cout << "Title: ";
-	std::getline(std::cin, transac.title);
-	std::cout << "Amount: ";
-	std::cin >> transac.amount;
+	int last_id = -1;
 
-	// Create transaction id
-	int id = 0;
-	transac.id = 0;
+	transaction transac = create_transact(last_id);
+	
+		// Print transaction
+	print_transact(std::cout, transac);
 
-	// Create transaction date
+	return 0;
+}
+
+/**
+ * Gets the current date on the system and returns a
+ * formatted string.
+ */
+std::string get_date() {
 	std::time_t t = std::time(0);
 	std::tm* now = std::localtime(&t);
+
 	int month = now->tm_mon + 1,
 			day = now->tm_mday,
 			year = now->tm_year + 1900;
-	transac.date_created = std::to_string(month) + "/" + std::to_string(day) + "/" + std::to_string(year);
 
-	// Print transaction
-	std::cout << "\n\nTRANSACTION (#" << transac.id << ") : "
-						<< "\nNAME: " << transac.title
-						<< "\nAMOUNT: " << transac.amount
-						<< "\nDATE_CREATED: " << transac.date_created;
+	std::string date = std::to_string(month) + '/' + std::to_string(day) + '/' + std::to_string(year);
 
-	return 0;
+	return date;
+}
+
+transaction create_transact(const int& last_id) {
+	transaction t;
+	std::cout << "NEW TRANSACTION\n" + std::string(15, '=');
+
+	std::cout << "(Title) ";
+	std::getline(std::cin, t.title);
+
+	std::cout << "(Amount) ";
+	std::cin >> t.amount;
+
+	t.id = last_id + 1;
+
+	t.date = get_date();
+}
+
+/**
+ * Prints out the data of a given transaction.
+ */
+void print_transact(std::ostream& os, const transaction& t) {
+	os << "\n\nTRANSACTION (#" << t.id << ") : "
+		 << "\nNAME: " << t.title
+		 << "\nAMOUNT: " << t.amount
+		 << "\nDATE_CREATED: " << t.date_created;
 }
 
