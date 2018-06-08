359d5ab9d84bfbbe9770f9101967e32a30e8675f
Adam Mahood <mahood.adam@gmail.com>
2018-06-08 10:44:42 -0400

    Add menu screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
+#include <string>
 
 #include "transaction.h"
 
 int main() {
+	const std::string header = "WELCOME TO TRANSACTION TRACKER";
+	const std::string sep(header.size(), '=');
+
+	std::cout << header << '\n' << sep
+		<< "(a) View transactions\n" << "(b) Add transaction\n"
+		<< "(c) Remove last transaction\n" << "(d) View net gain/loss\n" << sep;
+
 	tt::Transaction transact { 1, "mcdonalds", 20.f };
 
 	transact.print();
-
-	std::cout << "ID: " << transact.get_id() << std::endl;
-	std::cout << "NAME: " << transact.get_name() << std::endl;
-	std::cout << "AMOUNT: " << transact.get_amount() << std::endl;
-	std::cout << "DATE: " << transact.get_date() << std::endl;
 
 	return 0;
 }
